2ec9862e78dfa8b432884ab40848e70739eb5fb9
Joschua H <joschua.hass.sh@gmail.com>
2022-07-03 14:57:42 +0200

    feat: Navbar & Home header

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,20 @@
+import {Box, Container, Typography} from "@mui/material";
+import {Navbar} from "../components/Navbar";
+
 export default function Home() {
   return (
-    <div></div>
+    <div>
+        <Navbar />
+        <div style={{background: "url('/img/food-1.jpg')", backgroundSize: "cover", backgroundPosition: "center",
+            backgroundRepeat: "no-repeat"}}>
+                <Box py={26} color="white" style={{backgroundColor: "rgba(0,0,0,0.25)"}}>
+                    <Typography variant="h3" align="center">Get some food. But the epic way.</Typography>
+                    <Typography align="center">Enjoy ordering your food as never before with FoodZone.</Typography>
+                </Box>
+        </div>
+        <div style={{height: "200vh"}}>
+
+        </div>
+    </div>
   )
 }
