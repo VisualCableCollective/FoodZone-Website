6a40c5d161ac6cb4a7491122eed33b620711076c
Joschua H <joschua.hass.sh@gmail.com>
2022-07-04 17:07:49 +0200

    feat: Google Places API

--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,10 @@ class MyDocument extends Document {
                         rel="stylesheet"
                         href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
                     />
+                    <script
+                        type="text/javascript"
+                        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC7yBiZnU4SohA7t59eWxFK4KLEsN51_9U&libraries=places"
+                    />
                 </Head>
                 <body>
                 <Main />
