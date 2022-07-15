c5a920f5be8a1020b3ca25f4cb3825f7ead7d618
Joschua H <joschua.hass.sh@gmail.com>
2022-07-15 12:07:06 +0200

    feat(delivery-services): card

--- pages/delivery-services/[address].js
+++ pages/delivery-services/[address].js
@@ -1,5 +1,6 @@
 import {MainLayout} from "../../layouts/MainLayout";
 import {useRouter} from "next/router";
+import {Box, Card, CardContent, CardMedia, Container, IconButton, Typography} from "@mui/material";
 
 export default function DeliveryServicesListPage() {
     const router = useRouter();
@@ -7,7 +8,26 @@ export default function DeliveryServicesListPage() {
 
     return (
         <MainLayout>
-            Your address is: {urlAddress}
+            <Container>
+                <Box pt={12} textAlign="center">
+                    Your address is: {urlAddress}
+                    <Card sx={{ display: 'flex' }} variant="outlined">
+                        <CardMedia
+                            component="img"
+                            sx={{ width: 151 }}
+                            image="/img/food-2.jpg"
+                            alt="Live from space album cover"
+                        />
+                        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                            <CardContent sx={{ flex: '1 0 auto' }}>
+                                <Typography component="div" variant="h5">
+                                    Alex's Burger Lounge
+                                </Typography>
+                            </CardContent>
+                        </Box>
+                    </Card>
+                </Box>
+            </Container>
         </MainLayout>
     )
 }
