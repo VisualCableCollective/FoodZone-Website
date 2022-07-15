fdcda0cd81c9ccb47a7b932ac114c48188d5f20c
Joschua H <joschua.hass.sh@gmail.com>
2022-07-15 09:33:04 +0200

    feat(order): redirect

--- pages/order.js
+++ pages/order.js
@@ -1,14 +1,21 @@
 import {Box, Container, Typography} from "@mui/material";
 import {MainLayout} from "../layouts/MainLayout";
 import {AddressInput} from "../components/AddressInput";
+import {useRouter} from "next/router";
 
 export default function OrderPage() {
+    const router = useRouter();
+
+    function onAddressSubmit(address) {
+        router.push("/delivery-services/" + encodeURIComponent(address));
+    }
+
     return (
         <MainLayout>
             <Container>
                 <Box pt={12} textAlign="center">
                     <Typography variant="h4">Order some food right here.</Typography>
-                    <AddressInput />
+                    <AddressInput onAddressSubmit={onAddressSubmit} />
                 </Box>
             </Container>
         </MainLayout>
