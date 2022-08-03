ddee362791ae0237a5b36caeb4b684ec8492c51e
Joschua H <joschua.hass.sh@gmail.com>
2022-08-03 09:50:46 +0200

    fix: double render in dev mode

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  // no double render in dev mode
+  reactStrictMode: false
 }
 
 module.exports = nextConfig
