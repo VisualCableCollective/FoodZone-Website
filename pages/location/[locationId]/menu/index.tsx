2cbecdd752d73f532723af3d999b14ccb5d4dbdc
Joschua H <joschua.hass.sh@gmail.com>
2022-08-04 15:03:02 +0200

    work sync

--- pages/location/[locationId]/menu/index.tsx
+++ pages/location/[locationId]/menu/index.tsx
@@ -1,14 +1,47 @@
 import { MainLayout } from "../../../../layouts/MainLayout";
-import {Box, Container} from "@mui/material";
+import {Box, Card, CardActionArea, CardContent, CardMedia, Container, Grid, Typography} from "@mui/material";
+import NearMeIcon from "@mui/icons-material/NearMe";
+import {blue} from "@mui/material/colors";
 
 export default function LocationMenuPage() {
   return (
     <MainLayout>
       <Container>
         <Box pt={4}>
-
+          <Grid container columns={3} spacing={1}>
+            <MenuCategory name="Burger" />
+            <MenuCategory name="Pizza" />
+            <MenuCategory name="Pasta" />
+          </Grid>
         </Box>
       </Container>
     </MainLayout>
+  )
+}
+
+function MenuCategory({name}) {
+  return (
+    <Grid item xs={1}>
+      <Card variant="outlined" sx={{position: "relative"}}>
+        <CardMedia
+          component="div"
+          sx={{ height: 200 }}
+          image="/img/food-2.jpg"
+        />
+        <Box sx={{background: "rgba(0,0,0,0.0)",
+          position: "absolute", top: 0, right: 0, zIndex: 10, width: "100%", height: 200, display: "flex", alignItems: "end",
+          transition: "background 0.2s ease-out", "&:hover": {
+            background: "rgba(0,0,0,0.2)"
+          }}}>
+          <Box sx={{background: "linear-gradient(0deg, rgba(0,0,0,0.6222864145658263) 0%, rgba(255,255,255,0) 100%)", paddingBottom: 1,
+            display: "flex", justifyContent: "start",
+            paddingRight: 1, paddingLeft: 1, paddingTop: 10, width: "100%"}}>
+            <Typography color="white" fontSize={24} fontWeight={500}>
+              {name}
+            </Typography>
+          </Box>
+        </Box>
+      </Card>
+    </Grid>
   )
 }
